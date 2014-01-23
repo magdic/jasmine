eb79552f4eb61305cd5c161f62aad85a15212d83
Magdiel <luisc@thehangar.cr>
2014-01-23 14:35:38 -0600

    UPload all the jasmine files

---
+++ spec/helloSpec.js
@@ -0,0 +1,10 @@
+describe("Title: Jasmine Test", function() {
+          it("It's search for vulgaries word", function() {
+            var badWords = ["caremoco", "morbo", "sadico", "necrofilia"];
+            for (var i = badWords.length - 1; i >= 0; i--) {
+              badWords[i];            
+              expect(helloWorld()).not.toContain(badWords[i]);
+            }
+          });
+});
+
---
+++ src/hello.js
@@ -0,0 +1,13 @@
+
+
+  var data = '{"comment": "La necrofilia y el caremoco morbo sadico son malas personas"}';
+ 
+	var json = JSON.parse(data);
+
+var message = json.comment;
+
+function helloWorld()
+{
+ 	return message;
+}
+
